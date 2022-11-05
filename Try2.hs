92a2493bc510a209779e5baaaccdb110055186ba
Mark Tullsen <tullsen@galois.com>
2022-11-05 15:10:55 -0700

    edits

--- Try2.hs
+++ Try2.hs
@@ -2,6 +2,7 @@
 
 module Try2 where
 
+import Control.Exception (assert)
 import Control.Arrow
 import Data.Functor
 
@@ -36,8 +37,8 @@ data MLRange  = MLRange  Int Int  -- max length range
 
 
 -- | fails unless we consume stream
-newtype FWParser  a = FWP (Stream -> Maybe a)
-                      deriving (Functor)
+data FWParser  a = FWP Int (Stream -> Maybe a)
+                   deriving (Functor)
 
 
 -- | need not consume stream
@@ -48,12 +49,14 @@ newtype DynParser a = DynP (Stream -> Maybe (a, DynRange))
   --   the stream
   -- a reader monad: as Stream doesn't change
 
+-- TODO: make monadic!
+
 -- stream operations
 
 getRngSt  :: DynRange -> Stream -> Stream  -- gets to EOF
 getRngSt = stub
 
-getFWRange  :: FWRange    -> Stream -> Stream
+getFWRange  :: FWRange -> Stream -> Stream
 getFWRange = stub
 
 
@@ -71,9 +74,15 @@ getFWRange = stub
 applyDynParser :: Stream -> DynParser a -> Maybe (a, DynRange)
 applyDynParser s (DynP p) = p s
 
-applyFWParser  :: Stream -> FWParser  a -> Maybe a
-applyFWParser s (FWP p) = p s
+applyFWParser  :: Int -> Stream -> FWParser  a -> Maybe a
+applyFWParser i s (FWP j p) = assert (j == length s)
+                            $ p s
 
+  -- i == j?
+  -- where to extract the 'j' width stream:
+  --   - right here or in caller!!
+  -- design is to extract out a fixed width stream! (rather than "i s")?
+  --  -- use bytestreams!!
                      
 ---- examples & testing ------------------------------------------------------
 
@@ -92,11 +101,33 @@ pY :: DynParser Y
 pY = Y <$> pInt
 
 pPY :: DynParser (R Y)
-pPY = stub
+pPY =  stub
 
+  -- AHA: This parses 4-bytes, then jumps to a dynamic location, so ...
+  --   - static parser (that calls dynamic)
+  --   - dynamic parser??
+  --   - MERGE these.
+  
+{-
+  pPY :: [Rng] -> Parser (R Y)
+  pPY [r] = do
+            offset <- pUint32 r
+            rNew   <- mkReg offset
+            y      <- pY rNew
+            return (R rNew y)
+-}
 
 ---- Prims -------------------------------------------------------------------
 
+type Int32 = Int  -- FIXME
+
+pInt32 :: FWParser Int32 -- binary, 4 bytes!
+pInt32 = FWP 4 $
+         \s-> case readsM s of   -- FIXME: not binary!!
+                Just (i,[]) -> Just i
+                _           -> Nothing -- leftover string is a fail.
+
+-- | ascii, dynamic number of bytes
 pInt :: DynParser Int
 pInt = DynP $
          \s-> case readsM s of
@@ -118,14 +149,3 @@ readsM s = case reads s of
              [(a,s')] -> Just (a,s')
              _        -> Nothing
 
-{-
-  pY  :: [Rng]  -> Parser Y
-  pY [r] = stub
-
-  pPY :: [Rng] -> Parser (R Y)
-  pPY [r] = do
-            offset <- pUint32 r
-            rNew   <- mkReg offset
-            y      <- pY rNew
-            return (R rNew y)
--}
