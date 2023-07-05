a2939ce584946d3a0c33eec48ddfdc084e707a7b
Sam Cowger <sam@galois.com>
2023-07-05 10:03:22 -0700

    Work on free variable detection and rely on it when delaying/forcing thunks

--- locc/src/Language/Optimal/Compile.hs
+++ locc/src/Language/Optimal/Compile.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TemplateHaskellQuotes #-}
@@ -33,15 +35,20 @@ compileBind modVars var expr = bindS (varP (mkName' var)) [|delayAction $(compil
 
 compileExpr :: Set Symbol -> Exp -> Q Exp
 compileExpr modVars expr =
-  case thunkBinds of
-    [] -> pure expr
-    _ -> doE (thunkBinds <> [noBindS (pure expr)])
-  where
-    thunkVars = free expr `Set.intersection` Set.map mkName' modVars
-    thunkBinds =
-      [ bindS (varP =<< newName (show thunkVar)) [|force $(varE thunkVar)|]
-        | thunkVar <- Set.toList thunkVars
-      ]
+  do
+    let modVarNames = Set.map mkName' modVars
+        thunkVarNames = freeVars expr `Set.intersection` modVarNames
+    thunkVarFreshNames <- sequence (Map.fromSet (newName . show) thunkVarNames)
+    thunkBinds <-
+      sequence
+        [ bindS (varP fresh) [|force $(varE original)|]
+          | (original, fresh) <- Map.toList thunkVarFreshNames
+        ]
+    let rename n = case thunkVarFreshNames Map.!? n of Just n' -> pure n'; Nothing -> pure n
+    expr' <- renameExp rename expr
+    case thunkBinds of
+      [] -> pure expr'
+      _ -> pure (DoE Nothing (thunkBinds <> [NoBindS expr']))
 
 -- - Collect all of the free variables in this expression that are also variables
 --   in the encompassing module
@@ -86,53 +93,198 @@ compileOptimalType pty =
 mkName' :: Text -> Name
 mkName' = mkName . Text.unpack
 
-free :: Exp -> Set Name
-free = freeWith mempty
+-------------------------------------------------------------------------------
 
-freeWith :: Set Name -> Exp -> Set Name
-freeWith binders expr =
+class Monoid c => Collection c e | c -> e where
+  member :: e -> c -> Bool
+  default member :: e -> c -> Bool
+  member e c = not (notMember e c)
+
+  notMember :: e -> c -> Bool
+  default notMember :: e -> c -> Bool
+  notMember e c = not (member e c)
+
+  insert :: e -> c -> c
+
+  singleton :: e -> c
+  default singleton :: e -> c
+  singleton e = insert e mempty
+
+  {-# MINIMAL (member | notMember), insert #-}
+
+-------------------------------------------------------------------------------
+
+newtype FreeVars = FreeVars (Set Name)
+  deriving (Show)
+
+newtype BindingVars = BindingVars (Set Name)
+  deriving (Show)
+
+instance Semigroup FreeVars where
+  FreeVars f1 <> FreeVars f2 = FreeVars (f1 <> f2)
+
+instance Semigroup BindingVars where
+  BindingVars f1 <> BindingVars f2 = BindingVars (f1 <> f2)
+
+instance Monoid FreeVars where
+  mempty = FreeVars mempty
+
+instance Monoid BindingVars where
+  mempty = BindingVars mempty
+
+instance Collection FreeVars Name where
+  member e (FreeVars fvs) = Set.member e fvs
+  insert e (FreeVars fvs) = FreeVars (Set.insert e fvs)
+
+instance Collection BindingVars Name where
+  member e (BindingVars bvs) = Set.member e bvs
+  insert e (BindingVars bvs) = BindingVars (Set.insert e bvs)
+
+-------------------------------------------------------------------------------
+
+freeVars :: Exp -> Set Name
+freeVars expr =
+  let FreeVars fvs = expFreeVars mempty expr
+   in fvs
+
+expFreeVars :: BindingVars -> Exp -> FreeVars
+expFreeVars bindings expr =
   case expr of
     VarE n
-      | n `Set.notMember` binders -> Set.singleton n
+      | n `notMember` bindings -> singleton n
       | otherwise -> mempty
     ConE n -> mempty
     LitE l -> mempty
-    AppE e1 e2 -> go e1 <> go e2
-    AppTypeE e t -> go e
-    InfixE e1M e2 e3M -> maybe mempty go e1M <> go e2 <> maybe mempty go e3M
-    UInfixE e1 e2 e3 -> go e1 <> go e2 <> go e3
-    ParensE e -> go e
-    LamE params body -> freeWith (foldMap matchedNames params) body
-    LamCaseE matches -> undefined
+    AppE e1 e2 -> goExp e1 <> goExp e2
+    AppTypeE e t -> goExp e
+    InfixE e1M e2 e3M -> maybe mempty goExp e1M <> goExp e2 <> maybe mempty goExp e3M
+    UInfixE e1 e2 e3 -> goExp e1 <> goExp e2 <> goExp e3
+    ParensE e -> goExp e
+    LamE params body -> goExp' (foldMap patBindings params) body
+    -- LamCaseE matches -> undefined
     -- LamCasesE matches -> undefined
-    TupE es -> foldMap go (catMaybes es)
-    UnboxedTupE es -> foldMap go (catMaybes es)
-    UnboxedSumE e _ _ -> go e
-    CondE e1 e2 e3 -> go e1 <> go e2 <> go e3
-    _ -> undefined
+    TupE es -> foldMap goExp (catMaybes es)
+    UnboxedTupE es -> foldMap goExp (catMaybes es)
+    UnboxedSumE e _ _ -> goExp e
+    CondE e1 e2 e3 -> goExp e1 <> goExp e2 <> goExp e3
+    MultiIfE matches -> foldMap (\(guard, e) -> undefined guard <> goExp e) matches
+    ListE es -> foldMap goExp es
+    LetE decs e -> foldMap goDec decs <> goExp' (foldMap decBindings decs) e
+    _ -> error (show expr)
   where
-    go = freeWith binders
+    goExp = expFreeVars bindings
+    goExp' bindings' = expFreeVars (bindings' <> bindings)
+    goDec = decFreeVars bindings
 
-matchedNames :: Pat -> Set Name
-matchedNames pat =
+guardFreeVars :: BindingVars -> Guard -> FreeVars
+guardFreeVars bindings guard =
+  case guard of
+    NormalG e -> expFreeVars bindings e
+    PatG stmts -> foldMap (stmtFreeVars bindings) stmts
+
+guardBindings :: Guard -> BindingVars
+guardBindings guard =
+  case guard of
+    NormalG _ -> mempty
+    PatG stmts -> foldMap stmtBindings stmts
+
+stmtFreeVars :: BindingVars -> Stmt -> FreeVars
+stmtFreeVars bindings stmt =
+  case stmt of
+    BindS pat e -> expFreeVars (bindings <> patBindings pat) e
+    LetS decs -> foldMap (decFreeVars bindings) decs
+    NoBindS e -> expFreeVars bindings e
+    ParS stmtss -> foldMap (foldMap (stmtFreeVars bindings)) stmtss
+    RecS stmts -> foldMap (stmtFreeVars bindings) stmts
+
+stmtBindings :: Stmt -> BindingVars
+stmtBindings stmt =
+  case stmt of
+    BindS pat _ -> patBindings pat
+    LetS decs -> foldMap decBindings decs
+    NoBindS _ -> mempty
+    ParS stmtss -> foldMap (foldMap stmtBindings) stmtss
+    RecS stmts -> foldMap stmtBindings stmts
+
+decBindings :: Dec -> BindingVars
+decBindings dec =
+  case dec of
+    ValD pat body _ -> patBindings pat
+    _ -> error (show dec)
+
+decFreeVars :: BindingVars -> Dec -> FreeVars
+decFreeVars bindings dec =
+  case dec of
+    ValD pat (NormalB e) _ -> expFreeVars (bindings <> patBindings pat) e
+    _ -> error (show dec)
+
+patBindings :: Pat -> BindingVars
+patBindings pat =
   case pat of
     LitP l -> mempty
-    VarP n -> Set.singleton n
-    TupP ps -> foldMap matchedNames ps
-    UnboxedTupP ps -> foldMap matchedNames ps
-    UnboxedSumP p _ _ -> matchedNames p
-    ConP _ _ ps -> foldMap matchedNames ps
+    VarP n -> singleton n
+    TupP ps -> foldMap patBindings ps
+    UnboxedTupP ps -> foldMap patBindings ps
+    UnboxedSumP p _ _ -> patBindings p
+    ConP _ _ ps -> foldMap patBindings ps
     -- this `n` represents, e.g., the `:` in a pattern like (x:xs)
-    InfixP p1 n p2 -> matchedNames p1 <> matchedNames p2
-    UInfixP p1 n p2 -> matchedNames p1 <> matchedNames p2
-    ParensP p -> matchedNames p
-    TildeP p -> matchedNames p
-    BangP p -> matchedNames p
-    AsP n p -> Set.insert n (matchedNames p)
+    InfixP p1 n p2 -> patBindings p1 <> singleton n <> patBindings p2
+    UInfixP p1 n p2 -> patBindings p1 <> singleton n <> patBindings p2
+    ParensP p -> patBindings p
+    TildeP p -> patBindings p
+    BangP p -> patBindings p
+    AsP n p -> insert n (patBindings p)
     WildP -> mempty
-    RecP recName fieldPats -> foldMap (\(n, p) -> matchedNames p) fieldPats
-    ListP ps -> foldMap matchedNames ps
-    SigP p _ -> matchedNames p
-    ViewP e p -> matchedNames p
+    RecP recName fieldPats -> foldMap (\(n, p) -> patBindings p) fieldPats
+    ListP ps -> foldMap patBindings ps
+    SigP p _ -> patBindings p
+    ViewP e p -> patBindings p
 
--- _ -> undefined
+-------------------------------------------------------------------------------
+
+renameExp :: (Name -> Q Name) -> Exp -> Q Exp
+renameExp f expr =
+  case expr of
+    VarE n -> VarE <$> f n
+    ConE n -> ConE <$> f n
+    LitE l -> pure expr
+    AppE e1 e2 -> appE (go e1) (go e2)
+    ParensE e -> parensE (go e)
+    CondE e1 e2 e3 -> condE (go e1) (go e2) (go e3)
+    LetE decs e -> letE (map goDec decs) (go e)
+    _ -> error $ "TODO: finish constructors in `renameExp` (failed on " <> take 30 (show expr) <> "...)"
+  where
+    -- TODO: etc
+
+    go = renameExp f
+    goDec = renameDec f
+
+renameDec :: (Name -> Q Name) -> Dec -> Q Dec
+renameDec f dec =
+  case dec of
+    ValD pat (NormalB e) wheres -> valD (renamePat f pat) (normalB (renameExp f e)) (map pure wheres)
+    _ -> error $ "TODO: finish constructors in `renameDec` (failed on " <> take 30 (show dec) <> "...)"
+
+renamePat :: (Name -> Q Name) -> Pat -> Q Pat
+renamePat f pat =
+  case pat of
+    LitP l -> litP l
+    VarP n -> VarP <$> f n
+    _ -> error $ "TODO: finish constructors in `renamePat` (failed on " <> take 30 (show pat) <> "...)"
+
+-- TupP ps -> foldMap patBindings ps
+-- UnboxedTupP ps -> foldMap patBindings ps
+-- UnboxedSumP p _ _ -> patBindings p
+-- ConP _ _ ps -> foldMap patBindings ps
+-- -- this `n` represents, e.g., the `:` in a pattern like (x:xs)
+-- InfixP p1 n p2 -> patBindings p1 <> singleton n <> patBindings p2
+-- UInfixP p1 n p2 -> patBindings p1 <> singleton n <> patBindings p2
+-- ParensP p -> patBindings p
+-- TildeP p -> patBindings p
+-- BangP p -> patBindings p
+-- AsP n p -> insert n (patBindings p)
+-- WildP -> mempty
+-- RecP recName fieldPats -> foldMap (\(n, p) -> patBindings p) fieldPats
+-- ListP ps -> foldMap patBindings ps
+-- SigP p _ -> patBindings p
+-- ViewP e p -> patBindings p
