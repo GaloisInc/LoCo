8b2e40ad0e44e15fcc72494754877a960a100b4b
Sam Cowger <sam@galois.com>
2023-07-11 10:40:51 -0700

    Add some testing of Optimal parsers

--- locc/test/Main.hs
+++ locc/test/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.Language.LoCoEssential.Essence qualified
+import Test.Language.Optimal.Parse qualified
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 main :: IO ()
@@ -10,5 +11,6 @@ tests :: TestTree
 tests =
   testGroup
     "main"
-    [ Test.Language.LoCoEssential.Essence.tests
+    [ Test.Language.LoCoEssential.Essence.tests,
+      Test.Language.Optimal.Parse.tests
     ]
---
+++ locc/test/Test/Language/Optimal/Parse.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Language.Optimal.Parse (tests) where
+
+import Data.Map qualified as Map
+import Data.Text (Text)
+import Language.Optimal.Parse
+import Language.Optimal.Syntax (Type (..))
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit
+
+tests :: TestTree
+tests =
+  testGroup
+    "Test.Language.Optimal.Parse"
+    [ varNameTests,
+      typeTests
+    ]
+
+testParseSuccess :: (Eq a, Show a) => Parser a -> Text -> a -> Assertion
+testParseSuccess p on expected =
+  case runParser p on of
+    Left err -> assertFailure err
+    Right actual -> actual @?= expected
+
+testParseFailure :: (Eq a, Show a) => Parser a -> Text -> Assertion
+testParseFailure p on =
+  case runParser p on of
+    Left err -> pure ()
+    Right actual -> assertFailure "parsing succeeded"
+
+varNameTests :: TestTree
+varNameTests =
+  testGroup
+    "variable names"
+    [ testSuccess "lowercase variable" "hello" "hello",
+      testSuccess "variable with internal uppercase" "hEllo" "hEllo",
+      testSuccess "variable with numerals" "hell0" "hell0",
+      testSuccess "variable with underscores" "he_llo" "he_llo",
+      testFailure "variable with initial uppercase" "Hello"
+    ]
+  where
+    testSuccess name source expected =
+      testCase name (testParseSuccess parseVarName source expected)
+    testFailure name source =
+      testCase name (testParseFailure parseVarName source)
+
+typeTests :: TestTree
+typeTests =
+  testGroup
+    "types"
+    [ testSuccess "int" "Int" int,
+      testSuccess "fn2" "Int -> Int" (Arrow int int),
+      testSuccess "fn3" "Int -> Int -> Int" (Arrow int (Arrow int int)),
+      testSuccess "rec" "{ foo : Int, bar : Int }" (Rec (Map.fromList [("foo", int), ("bar", int)])),
+      testFailure "lowercase type" "int",
+      testFailure "unterminated record" "{ foo : Int "
+    ]
+  where
+    int = Alias "Int"
+    testSuccess name source expected =
+      testCase name (testParseSuccess parseOptimalType source expected)
+    testFailure name source =
+      testCase name (testParseFailure parseOptimalType source)
