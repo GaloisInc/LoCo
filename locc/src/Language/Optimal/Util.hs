bfe3a80ea7775561f369b89b53b4971aed23a055
Sam Cowger <sam@galois.com>
2023-09-01 16:36:11 -0700

    Utilities and missing cabal file module

---
+++ locc/src/Language/Optimal/Util.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Language.Optimal.Util where
+
+import Data.String (IsString (..))
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Language.Haskell.TH (Name, mkName)
+
+class Named a where
+  name :: a -> Name
+
+instance Named String where
+  name = mkName
+
+instance Named Text where
+  name = mkName . Text.unpack
+
+instance IsString Name where
+  fromString = mkName
