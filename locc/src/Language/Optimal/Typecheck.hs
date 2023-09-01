6fc5505aa41ad3012fd109a9d68206c170decddf
Sam Cowger <sam@galois.com>
2023-09-01 16:18:39 -0700

    Generalize type expansion/elaboration

--- locc/src/Language/Optimal/Typecheck.hs
+++ locc/src/Language/Optimal/Typecheck.hs
@@ -1,21 +1,17 @@
 module Language.Optimal.Typecheck where
 
 import Data.Map qualified as Map
-import Language.Optimal.Syntax
+import Language.Optimal.Syntax (Env, Type (..))
 
--- | For modules declared with a type alias, inline the types in the alias to
--- the greatest extent possible in the given type declaration environment
-expandType :: [TypeDecl] -> ModuleDecl -> Type
-expandType tyDecls ModuleDecl {..} = expandTy modTy
+-- | Recursively elaborate all aliases in a type according to the provided
+-- declaration map
+expandType :: Env Type -> Type -> Type
+expandType tyEnv ty =
+  case ty of
+    Alias alias -> maybe ty go (tyEnv Map.!? alias)
+    Arrow t1 t2 -> Arrow t1 (go t2)
+    Tuple ts -> Tuple (map go ts)
+    List t -> List (go t)
+    Rec s env -> Rec s (go <$> env)
   where
-    expandTy ty =
-      case ty of
-        Alias alias -> maybe ty expandTy (tyEnv Map.!? alias)
-        Arrow t1 t2 -> Arrow t1 $ expandTy t2
-        Tuple ts -> Tuple $ map expandTy ts
-        List t -> List $ expandTy t
-        Rec env -> Rec $ expandTy <$> env
-    tyEnv = Map.fromList [(tdName, tdType) | TypeDecl {..} <- tyDecls]
-
-checkModule :: ModuleDecl -> Either String ModuleDecl
-checkModule ModuleDecl {..} = undefined
+    go = expandType tyEnv
