c383aee04e7630564c30aa232550c1eabfb7fcb9
Sam Cowger <sam@galois.com>
2023-11-09 09:44:01 -0800

    Use Optimal vectors to parse ICC tagged data

--- optimal-ddl/src/Language/Optimal/Samples/Daedalus.hs
+++ optimal-ddl/src/Language/Optimal/Samples/Daedalus.hs
@@ -1,152 +1,164 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -ddump-splices #-}
 
 module Language.Optimal.Samples.Daedalus where
 
 import Control.Monad.IO.Class (MonadIO (..))
-import Daedalus (pBEUInt32, pChunk, pSetStreamAt)
+import Daedalus (pBEUInt32)
 import Daedalus.RTS.Convert (convert)
-import Daedalus.RTS.Input (Input, newInput)
+import Daedalus.RTS.Input (Input, inputDrop, inputTake, newInput)
+import Daedalus.RTS.Numeric (SizeType)
 import Daedalus.RTS.Vector qualified as DDL
 import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
 import Data.List.NonEmpty qualified as NE
 import ICC
 import Language.Optimal.Quote (optimal)
-import RTS (DDL, UInt, fromUInt)
-import RTS.Annot (Annotation)
+import RTS (Arith (..), UInt, fromUInt, lit)
 import RTS.ParseError (ErrorStyle (MultiError), HasSourcePaths)
 import RTS.Parser (ParserG, runParser)
-import RTS.ParserAPI (BasicParser (pPeek), ResultG (..))
+import RTS.ParserAPI (ResultG (..))
+import System.IO.Unsafe (unsafePerformIO)
 import Thunk.RefVal
 import Thunk.Vector
 
+-- Now that I've done this, what about something a little different?
+--
+-- Thought about applying Optimal types on top of *all* daedalus types, but that
+-- would require assuming a lazy source language. We'd construct an `ICC` as
+-- Daedalus understands it, then populate Optimal-declared subfields by
+-- deconstructing it. But, we don't want to evince that whole Haskell value for
+-- ICC, because Daedalus doesn't do anything fancy to keep that construction
+-- lazy (as far as I know? TODO)
+
 mkOptimalICC :: MonadIO m => ByteString -> m (OptimalICC m)
-mkOptimalTagTable :: MonadIO m => ByteString -> m (OptimalTagTable m)
-mkOptimalTag :: MonadIO m => Input -> m (OptimalTag m)
+mkOptimalTagTable :: MonadIO m => Input -> m (OptimalTagTable m)
+mkOptimalTag :: (MonadIO m, Integral a) => a -> Input -> m (OptimalTag m)
+getTagTableElem :: MonadIO m => OptimalICC m -> Int -> m (OptimalTag m)
 
 type TagTable = DDL.Vector Tag
 
 type UInt32 = UInt 32
 
+-- could be nice to make `generate` accept anything `Integral`
+
+instance SizeType n => Num (UInt n) where
+  (+) = add
+  (-) = sub
+  (*) = mul
+  abs = id
+  signum 0 = 0
+  signum _ = 1
+  fromInteger = lit
+
+inputSlice :: UInt 64 -> UInt 64 -> Input -> Input
+inputSlice begin end input = inputTake (end - begin) (inputDrop begin input)
+
 [optimal|
-type OptimalICC = { pHeader : ProfileHeader, tTable  : TagTable }
+type OptimalICC = { profileHeader : ProfileHeader, tagTable : OptimalTagTable }
 
 mkOptimalICC : Source -> OptimalICC
 mkOptimalICC source = {
-  originalInput           = {| newInput "<filename>" source |},
-  pHeaderAndModifiedInput = <| parseProfileHeader' originalInput |>,
-  pHeader                 = {| fst pHeaderAndModifiedInput |},
+  origInput = {| newInput "<filename>" source |},
 
-  modifiedInput           = {| snd pHeaderAndModifiedInput |},
-  tTable                  = <| parseTagTable modifiedInput originalInput |>,
+  profileHeaderInput = {| inputTake 128 origInput |},
+  profileHeader = <| parseProfileHeader profileHeaderInput |>,
+
+  tagTableInput = {| inputDrop 128 origInput |},
+  tagTable = module mkOptimalTagTable tagTableInput,
 }
 
 type OptimalTagTable = { ttLen : UInt32, ttElems : [OptimalTag] }
 
-type OptimalTag = { eSig : UInt32, eOffset : UInt32, eSize : UInt32, eElem : Tag }
+mkOptimalTagTable : Input -> OptimalTagTable
+mkOptimalTagTable input = {
+  ttLenInput = {| inputTake 4 input |},
+  ttLen = <| parseU32 ttLenInput |>,
 
-
-mkOptimalTagTable : Source -> OptimalTagTable
-mkOptimalTagTable source = {
-  originalInput           = {| newInput "<filename>" source |},
-  pHeaderAndModifiedInput = <| parseProfileHeader' originalInput |>,
-  -- pHeader                 = {| fst pHeaderAndModifiedInput |},
-
-  modifiedInput           = {| snd pHeaderAndModifiedInput |},
-  ttLen                   = <| parseU32 modifiedInput |>,
-  ttLenInt                = {| fromIntegral (fromUInt ttLen) |},
-  ttElems                 = generate ttLenInt <| \_ -> undefined |>,
+  ttLenInt = {| fromIntegral (fromUInt ttLen) |},
+  ttElemsInput = {| inputDrop 4 input |},
+  ttElems = generate ttLenInt <| \i -> mkOptimalTag i ttElemsInput |>,
 }
 
-mkOptimalTag : Input -> OptimalTag
-mkOptimalTag input = {
-  eSigAndRest = <| parseU32' input |>,
-  eSig = {| fst eSigAndRest |},
-  eSigOutput = {| snd eSigAndRest |},
 
-  eOffsetAndRest = <| parseU32' eSigOutput |>,
-  eOffset = {| fst eOffsetAndRest |},
-  eOffsetOutput = {| snd eOffsetAndRest |},
+type OptimalTag = { eSig : UInt32, eOffset : UInt32, eSize : UInt32, eElem : Tag }
 
-  eSizeAndRest = <| parseU32' eOffsetOutput |>,
-  eSize = {| fst eSizeAndRest |},
-  eSizeOutput = {| snd eSizeAndRest |},
+mkOptimalTag : Int -> Input -> OptimalTag
+mkOptimalTag idx input = {
+  eHeaderSize = {| 12 |},
+  eHeaderBegin = {| fromIntegral idx * eHeaderSize |},
+  eHeaderEnd = {| eHeaderBegin + eHeaderSize |},
+  eHeaderInput = {| inputSlice eHeaderBegin eHeaderEnd input |},
 
-  eElem = <| parseTag eSizeOutput eSig eOffset eSize |>,
+  eSigInput = {| inputSlice 0 4 eHeaderInput |},
+  eSig = <| parseU32 eSigInput |>,
+
+  eOffsetInput = {| inputSlice 4 8 eHeaderInput |},
+  eOffset = <| parseU32 eOffsetInput |>,
+
+  eSizeInput = {| inputSlice 8 12 eHeaderInput |},
+  eSize = <| parseU32 eSizeInput |>,
+
+  -- Our `input` elides the 132 bytes that comprise the ProfileHeader and tag
+  -- table length, so we adjust our offsets accordingly
+  eElemBegin = {| convert eOffset - 132 |},
+  eElemEnd = {| eElemBegin + convert eSize |},
+  eElemInput = {| inputSlice eElemBegin eElemEnd input |},
+  eElem = <| parseTag eSig eElemInput |>,
+}
+
+
+getTagTableElem : OptimalICC -> Int -> OptimalTag
+getTagTableElem icc idx = {
+  tagTable = icc.tagTable,
+  ttElems = tagTable.ttElems,
+  ttElem = index ttElems idx,
+
+  eSig = ttElem.eSig,
+  eOffset = ttElem.eOffset,
+  eSize = ttElem.eSize,
+  eElem = ttElem.eElem,
 }
 |]
 
--- We've manually pushed Optimal "up"/"through" some Daedalus-generated values,
--- but what about doing it automatically?
---
-
--- | Run a daedalus parser on some input. May throw IO error on parse failure.
-runDaedalus :: (MonadIO m, HasSourcePaths e, Show e) => ParserG e a -> Input -> m a
-runDaedalus parser input =
+debugTagTableElem :: OptimalICC IO -> Int -> IO ()
+debugTagTableElem icc idx =
   do
-    liftIO (putStrLn "parsing")
+    OptimalTag {..} <- getTagTableElem icc idx
+    force eSig >>= display "sig: "
+    force eOffset >>= display "offset: "
+    force eSize >>= display "size: "
+    force eElem >>= display "elem: "
+  where
+    display msg x = putStr msg >> print x
+
+parseProfileHeader :: MonadIO m => Input -> m ProfileHeader
+parseProfileHeader = runDaedalus "parsing profile header" pProfileHeader
+
+parseTag :: MonadIO m => UInt32 -> Input -> m Tag
+parseTag sig = runDaedalus "parsing tag data" (pTag sig)
+
+parseU32 :: MonadIO m => Input -> m UInt32
+parseU32 = runDaedalus "parsing u32" pBEUInt32
+
+-- | Run a daedalus parser on some input. Throws IO error on parse failure.
+runDaedalus :: (MonadIO m, HasSourcePaths e, Show e) => String -> ParserG e a -> Input -> m a
+runDaedalus msg parser input =
+  do
+    liftIO (putStrLn msg)
     case runParser parser MultiError input of
       NoResults errs -> err (take 1000 (show errs) <> "...")
       Results xs ->
         case NE.uncons xs of
-          ((x, itrace), Nothing) -> pure x
+          ((x, _inputTrace), Nothing) -> pure x
           (_, Just _) -> err "ambiguous"
   where
     err s = liftIO (fail s)
 
--- | A utility to produce a parse result as well as the (likely modified) parse
--- input (as in, source text) as it exists after executing that parse.
---
--- Motivation: be able to use `pTagTable` with original input as explicit
--- parameter and `pProfileHeader`-modified input as implicit* parameter
--- - *: not in the Daedalus sense, as in `?parameter`, but in the sense of the
---   input that's implicitly carried from one parser to another in a sequential
---   paradigm
---
--- Does this have the potential to introduce data dependencies where none
--- otherwise need exist?
--- - Probably not, since this input is carried from parse to parse anyway - the
---   only thing special about `pTagTable` is that it accepts an explicit stream
---   parameter at all...
---
--- Corollary: this might actually warrant tuple syntactic sugar?
-runDaedalus' :: (MonadIO m, HasSourcePaths e, Show e) => ParserG e a -> Input -> m (a, Input)
-runDaedalus' parser = runDaedalus parser'
-  where
-    -- parse, then peek
-    parser' = (,) <$> parser <*> pPeek
-
-parseProfileHeader :: MonadIO m => Input -> m ProfileHeader
-parseProfileHeader = runDaedalus pProfileHeader
-
-parseProfileHeader' :: MonadIO m => Input -> m (ProfileHeader, Input)
-parseProfileHeader' = runDaedalus' pProfileHeader
-
-parseTagTable :: MonadIO m => Input -> Input -> m TagTable
-parseTagTable modifiedInput originalInput = runDaedalus (pTagTable originalInput) modifiedInput
-
-parseU32 :: MonadIO m => Input -> m UInt32
-parseU32 = runDaedalus pBEUInt32
-
-parseU32' :: MonadIO m => Input -> m (UInt32, Input)
-parseU32' = runDaedalus' pBEUInt32
-
-runDaedalusAt :: (MonadIO m, DDL a) => Input -> UInt32 -> UInt32 -> ParserG Annotation a -> m a
-runDaedalusAt input offset size parser = runDaedalus seekAndParse input
-  where
-    seekAndParse = pSetStreamAt offset' input >> pChunk size' parser
-    offset' = convert offset
-    size' = convert size
-
-parseTag :: MonadIO m => Input -> UInt32 -> UInt32 -> UInt32 -> m Tag
-parseTag input sig offset size = runDaedalusAt input offset size (pTag sig)
-
--- It would be neat if we could have a monad that does `ParserG` actions as well
--- as IO - morally, to use `ParserG` as the Optimal monad. To accomplish this,
--- we'd need something that is like `ParserG` but happens to be `MonadIO`, and
--- maybe we'd need to be constantly lifting `ParserG` actions into this monad.
-
-parseICC :: MonadIO m => Input -> m ICC
-parseICC = runDaedalus pICC
+-- for debugging's sake
+src :: ByteString
+src = unsafePerformIO (BS.readFile "sRGB_D65_colorimetric.icc")
+{-# NOINLINE src #-}
