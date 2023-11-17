ac518a2201383737296dc19ef6c396cc33314c3e
Mark Tullsen <tullsen@galois.com>
2023-11-17 07:50:30 -0800

    pear: add tests

---
+++ pear/src/Language/LR/Examples/ICC_Tests.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
+module Language.LR.Examples.ICC_Tests where
+
+-- local modules:
+import           Language.LR.Examples.ICC_Spec
+import           Language.OptimalPEAR.Examples.ICC_Inputs
+
+import           Language.LR.API
+-- import           Language.PEAR.Types
+-- import           Language.PEAR.Util
+
+-- runTest :: Monad m => Contents -> m (Possibly [TED])
+runTest s = runPT s icc
+
+runtestsG = mapM_ (\i -> runTest i >>= print) ds
+
+runtestsE = mapM_ (\i -> runTest i >>= print) es
