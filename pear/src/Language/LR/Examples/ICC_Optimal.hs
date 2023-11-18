36de96c209f81ad480f4e3e52a2278c6c7380ffe
Mark Tullsen <tullsen@galois.com>
2023-11-17 21:50:49 -0800

    fix latest `icc` and code to test+demo it

--- pear/src/Language/LR/Examples/ICC_Optimal.hs
+++ pear/src/Language/LR/Examples/ICC_Optimal.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE LiberalTypeSynonyms #-}
 
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Language.LR.Examples.ICC_Optimal where
 
 -- base pkgs:
+import           Control.Monad.IO.Class
 import           Data.Word
+
+-- transformer pkg:
 
 -- package optimal:
 import           Language.Optimal.Quote (optimal)
@@ -17,10 +21,14 @@ import           Thunk.Vector
 -- local PEAR modules:
 import           Language.LR.Examples.ICC_Spec
 import           Language.LR.API
--- import           Language.PEAR.Util
+import           Language.PEAR.Util
 import qualified Language.PEAR.Region.API as R -- region
 import           Language.PEAR.Region.API(Region(..))
 
+import           Language.OptimalPEAR.RunOptimal
+
+-- bring into scope for demo:
+import           Language.OptimalPEAR.Examples.ICC_Inputs
 
 ---- ICC : Optim(l-PEAR) --------------------------------------
 
@@ -29,18 +37,22 @@ import           Language.PEAR.Region.API(Region(..))
 --  - no support for nested tuple patterns: (?)
 --      , ((a,b),c)    = <| return ((1,2),3) |>
 --  - no support for '_' bindings.
+--  - cannot inline type TBL
+
+type TBL = [(Word32,Word32)]
 
 [optimal|
 type ICC = { cnt       : VR Int
            , rRest     : Region
+           , tbl       : VR TBL
            , teds      : [TED]
            , teds_safe : [TED]
            }
 
 icc : Region -> ICC
 icc rFile =
-  { (cnt,rRest) = <| pInt4Bytes                  @! rFile          |>
-  , tbl         = <| pManySRPs (v cnt) pTblEntry @$ rRest          |>
+  { (cnt,rRest) = <| pInt4Bytes                     @!  rFile      |>
+  , tbl         = <| pManySRPs (v cnt) pTblEntry    @!- rRest      |>
   , rsTeds      = <| except $ mapM (getSubRegion rFile) (v tbl)    |>
   , teds        = <| mapM applyPTED rsTeds                         |>
   , crsFile     = <| makeCanonicalRegions (r cnt : r tbl : rsTeds) |>
@@ -50,3 +62,20 @@ icc rFile =
                        else throwE ["teds not safe"] |>
   }
 |]
+
+-- icc :: MonadIO m => Region -> PT m (ICC (PT m))
+
+---- Demo ----------------------------------------------------------
+
+run_ICC_d1 = run_ICC d1
+
+run_ICC = run' runPT' icc iccPrims ["cnt","rRest","tbl","teds","teds_safe"]
+
+iccPrims :: MonadIO m => [(String, ICC m -> m String)]
+iccPrims =
+  [ ("cnt"       , forceAndShow . cnt)
+  , ("rRest"     , forceAndShow . rRest)
+  , ("tbl"       , forceAndShow . tbl)
+  , ("teds"      , forceAndShow . teds)
+  , ("teds_safe" , forceAndShow . teds_safe)
+  ]
